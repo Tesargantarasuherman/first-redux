cd28f90dd78b496c4387ddee901a43f937a7d9c0
unknown <tgantara@gmail.com>
2022-06-21 13:27:31 +0700

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,36 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
-export const selectSong = (song)=>{
+
+export const selectSong = (song) => {
     // return an action
-    return{
-        type :'SONG_SELECTED',
+    return {
+        type: 'SONG_SELECTED',
         payload: song
     };
 };
-export const fetchPosts = ()=> async dispatch => {
-    const response = await jsonPlaceholder.get('/posts');
 
-    dispatch({type:'FETCH_POSTS',payload: response.data})
-};
-
-export const fetchUser =id=> async dispatch=>{
-    const response = await jsonPlaceholder.get(`/users/${id}`);
-    dispatch({type:'FETCH_USER',payload:response.data})
+export const fetchPostAndUsers =()=>async dispatch=>{
+    
 }
 
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+
+    dispatch({ type: 'FETCH_POSTS', payload: response.data })
+};
+
+export const fetchUser = id => async dispatch=>{
+    _fetchUser(id,dispatch);
+}
+// memoize
+// export const fetchUser = function (id) {
+//     return _.memoize(async function (dispatch) {
+//         const response = await jsonPlaceholder.get(`/users/${id}`);
+//         dispatch({ type: 'FETCH_USER', payload: response.data });
+//     });
+// };
+
+const _fetchUser = _.memoize(async (id,dispatch)=>{
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({type:'FETCH_USER',payload:response.data})
+});
